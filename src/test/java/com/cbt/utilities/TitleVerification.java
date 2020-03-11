614ca8f5530dfc47342b2d6463b9507be560e19a
isilg <isilavunduk@gmail.com>
2020-03-10 22:28:54 -0400

    hmw

---
+++ src/test/java/com/cbt/utilities/TitleVerification.java
@@ -0,0 +1,45 @@
+package com.cbt.utilities;
+
+import org.openqa.selenium.WebDriver;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TitleVerification {
+
+    public static void main(String[] args) throws Exception {
+        List<String> urls = Arrays.asList("http://practice.cybertekschool.com",
+                "http://practice.cybertekschool.com/dropdown",
+                "http://practice.cybertekschool.com/login");
+
+        WebDriver driver = BrowserFactory.getDriver("chrome");
+
+
+        List<String> sameTitleList= new ArrayList<>();
+
+        Thread.sleep(3000);
+        for (String url : urls)
+        {
+            driver.get(url);
+            sameTitleList.add(driver.getTitle());
+        }
+
+
+        for (int i = 0; i <sameTitleList.size()-1 ; i++) {
+            StringUtility.verifyEquals(sameTitleList.get(i),sameTitleList.get(i+1)); // i=2 -> i <titleList.size() -> titleList.get(3) out of range
+        }
+
+
+        List<Boolean> urlList= new ArrayList<>();
+        Thread.sleep(3000);
+
+        for (String url: urls) {
+
+            urlList.add(url.startsWith("http://practice.cybertekschool.com"));
+        }
+
+
+        driver.quit();
+    }
+
+ }
