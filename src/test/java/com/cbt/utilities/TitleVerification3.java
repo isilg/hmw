2917d520604b6685685b040c0817ee07040c4c68
isilg <isilavunduk@gmail.com>
2020-04-29 20:46:26 -0400

    hmw

--- src/test/java/com/cbt/utilities/TitleVerification3.java
+++ src/test/java/com/cbt/utilities/TitleVerification3.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class TitleVerification3 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
 
         List<String> urls = Arrays.asList("https://luluandgeorgia.com","https://wayfair.com/",
                                           "https://walmart.com", "https://westelm.com/");
@@ -14,11 +14,13 @@ public class TitleVerification3 {
         for (String url: urls){
             WebDriver driver= BrowserFactory.getDriver("chrome");
             driver.get(url);
+            Thread.sleep(2);
             if(url.contains(driver.getTitle().replace(" ","").toLowerCase())){
                 System.out.println("TEST PASSED!");
             }else{
                 System.out.println("TEST FAILED!");
             }
+            Thread.sleep(2);
             driver.quit();
         }
 
